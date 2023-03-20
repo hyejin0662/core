e12cd0cc80c9eb0e2fa0cba5ea9a7a927313deb4
hyejin0662 <hyejin0776@gmail.com>
2023-03-20 16:17:12 +0900

    004)싱글톤_컨테이너

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -17,17 +17,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository() 생성
+    // @Bean orderService -> new MemoryMemberRepository() 생성
+
+    // call AppConfig.memberService 출력
+    // call AppConfig.memberRepository 출력
+    // call AppConfig.memberRepository 출력
+
+    // call AppConfig.orderService 출력
+    // call AppConfig.memberRepository 출력
+
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
